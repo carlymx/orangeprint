77d1e4f6bc3e2990d4edc3b04bbf3fe34479c272
carlymx <carlymx@gmail.com>
2017-11-20 00:43:19 +0100

    add

---
+++ future/notes/03 - Cosas que faltan.sh
@@ -0,0 +1,74 @@
+
+		*** COSAS QUE FALTAN ****
+		
+	+ CONTROL POR LA SALIDA DE SERVOS DEL VENTILADOR DE CAPA, LUCES...
+	+ CON TRANSISTOR 2N2222
+	https://hetpro-store.com/TUTORIALES/transistor-2n2222-interruptor/?PageSpeed=noscript	
+
+	
+	+ CONTROL DEL VENTILADOR DE LA CAJA POR TEMPERATURA
+		- 2 OPCIONES:	1º CONTROLADO POR UN ARDUINO TINY85 + SENSOR DE TEMPERATURA + 2N2222
+						2ª CONTROL POR CONEXION AUXILIAR DE LA RAMPS V1.4 + MOD MARLIN PARA LEER TERMISTOR
+						   Y ADECUAR LA SALIDA DE POTENCIA DEL VENTILADOR.
+						   http://www.reprap.org/wiki/Custom_GCODES_con_los_pines_AUX_de_la_tarjeta_RAMPS.
+						   https://lh3.googleusercontent.com/proxy/jUKopIgkMZuFOqi5ZZl7LVY4l7j73bBxImAgH6FUNz7K2K7Ysetx5d17O-VudJtBdz2VJ83ncMHqhropxkvy_pr9QNZORDaNnHcpzqfPQalStU3hpiwD=w5000-h5000
+						
+		
+
+	+ 2ª impresora:
+	su octoprint -c 'octoprint --daemon start --port 5001 --pid ~/tmp/octoprint2 --basedir ~/.octoprint2'
+	sudo update-rc.d octoprint2.sh defaults 
+
+	o asi?
+	
+		sudo su octoprint
+	octoprint --daemon start --port 5001 --pid /tmp/octoprint2
+	
+	---------------------------------------------------------------
+	
+
+	+ Liberar Espacio
+		https://www.ramonramon.org/blog/2015/11/03/liberar-espacio-en-la-particion-de-sistema-ubuntu/
+
+	+ GPIO
+		http://www.orangepi.org/orangepibbsen/forum.php?mod=viewthread&tid=1308&highlight=gpio
+		https://diyprojects.io/orange-pi-armbian-install-wiringop-library-wiringpi-equivalent/#.WZf5iuntbRZ
+	
+		WIRINGOP (MPU H3 y H5)
+		https://github.com/kazukioishi/WiringOP
+			Instalacion
+			OPI H3:		git clone https://github.com/kazukioishi/WiringOP.git -b h3
+			OPI H5:		git clone https://github.com/kazukioishi/WiringOP.git -b h5
+
+		WIRINGOP-Zero (MPU H2)
+		https://github.com/xpertsavenue/WiringOP-Zero
+			Instalacion
+			OPI H2 (Zero): git clone https://github.com/xpertsavenue/WiringOP-Zero.git
+		
+
+// xx: INSTALAR CURA-ENGINE
+	DESDE REPOSITORIO:
+		https://launchpad.net/~thopiekar/+archive/ubuntu/cura
+			sudo add-apt-repository ppa:thopiekar/cura
+						
+						
+
+
+
+URL DE INFORMACION
+	OrangePi Plus OctoPrint and 3D Printer
+	http://www.orangepi.org/orangepibbsen/forum.php?mod=viewthread&tid=594
+	
+	[Question] Arduino IDE in Orange Pi Zero
+	https://forum.armbian.com/index.php?/topic/5161-question-arduino-ide-in-orange-pi-zero/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ future/notes/Samba Orange Pi Zero + Octoprint.sh
@@ -0,0 +1,158 @@
+	+ 1000 PLUGINS
+	---------------
+	-	TouchUI
+		http://plugins.octoprint.org/plugins/touchui/
+	-	CustomBackground
+		http://plugins.octoprint.org/plugins/custombackground/
+	-	
+	
+	
+
+//------------------------------------------------------//
+//		MONTAR UNIDADES DE RED UBUNTU EN WINDOWS		//
+//------------------------------------------------------//
+
+su octoprint
+
+sudo apt-get install samba 
+sudo apt-get install samba-common-bin
+
+mkdir /home/octoprint/share
+
+sudo nano /etc/samba/smb.conf
+
+	wins support = yes
+		
+	[opiz_share]
+	comment= Carpeta compartida
+	path= /home/octoprint/share
+	browseable= Yes
+	writeable= Yes
+	read only = no
+	guest ok = Yes
+	only guest= no
+	create mask= 0777
+	directory mask= 0777
+	public= no
+	write list = root, bananika, octoprint
+
+	[opiz_upload]
+	comment= Carpeta Upload
+	path= /home/octoprint/.octoprint/uploads
+	browseable= Yes
+	writeable= Yes
+	read only = no
+	guest ok = Yes
+	only guest= no
+	create mask= 0777
+	directory mask= 0777
+	public= no
+	write list = root, bananika, octoprint
+
+	[timelapse]
+	comment= camara timelapses
+	path= /home/octoprint/.octoprint/timelapse
+	browseable= Yes
+	writeable= Yes
+	read only = no
+	guest ok = Yes
+	only guest= no
+	create mask= 0777
+	directory mask= 0777
+	public= no
+	write list = root,bananika, octoprint
+
+ 
+sudo smbpasswd -a octoprint
+ 
+/etc/init.d/samba status
+/etc/init.d/samba restart
+
+---
+
+//------------------------------------------------------//
+//		MONTAR UNIDADES DE RED WINDOWS EN UBUNTU		//
+//------------------------------------------------------//
+
+// montar unidades de red Windows en Ubuntu
+// comprobar si es necesario que sea el usuario quien monte la unidad o si hay que dar permisos al directorio
+// por cuestiones de pribilegios...
+
+	chmod 777 /home/octoprint/.octoprint/uploads/shared
+
+	// COMANDO mount.cifs
+	mount.cifs //192.168.0.100/samba -o username=CaRLy,password=@1Merdolfo1@ /home/octoprint/samba_pccarly
+
+	mount.cifs //192.168.0.100/Proyectos /home/octoprint/.octoprint/uploads/shared -o username=carlymx@hotmail.com,password=@1Merdolfo1@ /home/octoprint/.octoprint/uploads/shared
+
+
+//--------- MONTAR UNIDAD DE RED PERMANENTEMENTE
+// http://eltallerdelbit.com/montar-unidades-de-red-windows-en-linux/
+
+	sudo apt-get install nfs-common
+	sudo apt-get install cifs-common o cifs-utils
+
+	sudo nano  /etc/fstab
+		//192.168.0.186/compartidolinux /media/compartidolinux cifs user=ubuntu,password=reverse,noexec,user,rw,nounix,iocharset=utf8 0 0
+
+ 
+
+
+
+
+
+
+
+/* otros
+mount -t cifs \\CARLYPC\samba -o username=CaRLy, password=contraseña /home/octoprint/samba_pccarly
+\\CARLYPC\samba
+
+sudo mount -t cifs //192.168.0.100/samba -o username=CaRLy,password=@1Merdolfo1@ /home/octoprint/samba_pccarly
+
+*/
+
+---
+
+//----------------------------------//
+//		CONTROL RELE POR GPIO		//
+//----------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
