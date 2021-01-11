e1325d8701fdeba32780f533b0495c49fa57619a
CaRLymx <carlymx@gmail.com>
2021-01-11 19:42:19 +0100

    Create orangeprint-help.sh
    prealpha

---
+++ scripts/orangeprint/orangeprint-help.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# orangeprint-help.sh - Un Archivo de ayuda.
+
+dialog --title "OrangePrint Help [v.0.0.1a]" \
+       --msgbox "Orangeprint:Este es un proyecto donde se intenta crear una com$
+sleep 1
+clear
