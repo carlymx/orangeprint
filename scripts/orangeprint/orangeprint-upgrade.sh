829cb04d16b76e41e6f457d8de118f8387046b64
CaRLymx <carlymx@gmail.com>
2021-01-11 19:47:55 +0100

    Create orangeprint-upgrade.sh
    prealpha

---
+++ scripts/orangeprint/orangeprint-upgrade.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# orangeprint-help.sh - Un Archivo de ayuda.
+
+dialog --title "OrangePrint Upgrade [v.0.0.1a]" \
+       --msgbox "Vamos a descargar los scripts de Orangeprint"
+
+sleep 1
+clear
+
+wget
+
+dialog --title "OrangePrint Upgrade [v.0.0.1a]" \
+       --msgbox "Scripts Actualizados."
+clear
