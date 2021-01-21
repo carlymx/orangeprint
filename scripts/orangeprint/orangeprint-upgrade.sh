b5b5c717acc1c73a06a0235dc763d12fd5838ad8
CaRLymx <carlymx@gmail.com>
2021-01-21 23:43:48 +0100

    Update orangeprint-upgrade.sh

--- scripts/orangeprint/orangeprint-upgrade.sh
+++ scripts/orangeprint/orangeprint-upgrade.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # orangeprint-upgrade.sh - Actualizar scripts orangeprint.
 
-dialog --title "OrangePrint Upgrade [v.0.0.1.2a]" \
+dialog --title "OrangePrint Upgrade [v.0.0.1.4a]" \
        --msgbox "Vamos a descargar los scripts de Orangeprint" 0 0
 
 sleep 1
 clear
-rm -frv orangeprint-upgrade.sh*
+rm -rfv orangeprint-upgrade*
 wget https://raw.githubusercontent.com/carlymx/orangeprint/master/scripts/orangeprint/orangeprint-upgrade.sh
 chmod +x orangeprint-upgrade.sh
 
-dialog --title "OrangePrint Upgrade [v.0.0.1.2a]" \
+dialog --title "OrangePrint Upgrade [v.0.0.1.4a]" \
        --msgbox "Scripts Actualizados." 0 0
 clear
