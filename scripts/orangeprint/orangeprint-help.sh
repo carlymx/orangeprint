13d5aaf61a2132358c208c31de49edae9edb64e0
CaRLymx <carlymx@gmail.com>
2021-01-22 12:48:06 +0100

    Update and rename orangeprint-help.sh to orangeprint-help

--- scripts/orangeprint/orangeprint-help.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# orangeprint-help.sh - Un Archivo de ayuda.
-
-dialog --title "OrangePrint Help [v.0.0.2a]" \
-       --msgbox "Orangeprint: Este es un proyecto donde se intenta crear una compilación de diferentes elementos para poder controlar impresoras RepRap con el software Octoprint en micro computadoras Orange Pi (Zero)."
-sleep 1
-clear
